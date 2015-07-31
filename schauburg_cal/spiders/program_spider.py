e241cc61de9de12a97d61615f9a868b4015589fe
Maximilian Stemmer-Grabow <dev@mxsg.de>
2015-07-31 17:16:52 +0200

    parse complete movie program

--- schauburg_cal/spiders/program_spider.py
+++ schauburg_cal/spiders/program_spider.py
@@ -1,10 +1,14 @@
 import scrapy
 import datetime
+import urlparse
+
+from schauburg_cal.items import MovieShowing
 
 class ProgramSpider(scrapy.Spider):
     name = "program"
     allowed_domains = ["schauburg.de"]
     start_urls = ["http://schauburg.de/programm.php"]
+    base_url = "http://schauburg.de"
 
     def parse(self, response):
         dates = response.xpath('/html/body/h5/text()').extract()
@@ -13,6 +17,11 @@ class ProgramSpider(scrapy.Spider):
         # print dateSelector.extract(), movieListSelector.extract()
 
         for dateText, movies in zip(dates, movieLists):
+
+            # construct scrapy item
+            showing = MovieShowing()
+
+
             # the date string is preceded by the day of the week
             # dateText should contain the date in "DD.MM." format
             dateText = dateText.split(',')[-1].strip().split('.')
@@ -31,4 +40,22 @@ class ProgramSpider(scrapy.Spider):
 
             # construct scrapy item
             date = dateThisYear if diffThisYear < diffNextYear else dateNextYear
-            print date
+            showing['date'] = date
+
+            movieEntrySelectors = movies.xpath('tr')
+
+            for movieEntry in movieEntrySelectors:
+
+                tableCells = movieEntry.xpath('td')
+
+                showingTime = tableCells.xpath('text()').extract_first().split('.')
+                showing['time'] = datetime.time(int(showingTime[0]), int(showingTime[1]))
+
+                showing['name'] = tableCells.xpath('a/text()').extract_first().strip()
+                # TODO: construct complete url, not only extension
+                showing['url'] = urlparse.urljoin(self.base_url,
+                        tableCells.xpath('a/@href').extract_first())
+
+                showing['comment'] = tableCells.xpath('i/text()').extract_first()
+
+                yield showing
